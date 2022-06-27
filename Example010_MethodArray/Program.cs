8fec37a34060b679cedc59f65e84bf4287e5b592
Trofimov Anton <Trofimovanton73@gmail.com>
2022-06-27 15:58:47 +0300

    add Example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 11, 22, 53, 14, 18, 51, 16, 17, 18 };
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прерываем искать дальше
+    }
+    //index=index+1;    
+    index++;
+}
